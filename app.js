7a8db369924bb27e634c2696f8d83d5a6935b41f
Freddy <freddychura93@gmail.com>
2025-05-10 11:39:11 -0400

    agregando rutas al archivo app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // app.js
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors()); // Permitir peticiones del frontend
-app.use(express.json()); // Parsear JSON del cuerpo
+app.use(cors());
+app.use(express.json());
 
-// (Las rutas se agregarán luego)
+// Rutas de usuario
+const rutasUsuario = require('./routes/usuarioRutas');
+app.use('/api/usuarios', rutasUsuario);
 
 module.exports = app;
